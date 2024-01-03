8142f548d32064bf078ac8c03d2b253fa24fba99
freeram <kulikovpavel2801@gmail.com>
2024-01-02 23:50:35 -0700

    Implement playRound

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -2,16 +2,48 @@
 function getComputerChoice() {
     // Return random integer - either 0, 1 or 2
     let choice = Math.floor(Math.random() * 3);
-    console.log(choice);
 
     switch (choice) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection.toLowerCase() === computerSelection) {
+        return "It's a tie!";
+    }
+
+    let output;
+    switch (playerSelection.toLowerCase()) {
+        case "rock":
+            output = (computerSelection === "paper") ? "You lose! Paper beats rock." : "You win! Rock beats scissors."
+            break;
+        case "paper":
+            output = (computerSelection === "scissors") ? "You lose! Scissors beat paper." : "You win! Paper beats rock."
+            break;
+        case "scissors":
+            output = (computerSelection === "rock") ? "You lose! Rock beats scissors." : "You win! Scissors beat paper."
+            break;
+        default:
+            output = "Incorrect player selection";
+    }
+    return output;
+}
+
+let computerChoice = getComputerChoice();
+console.log(`Computer choice: ${computerChoice}`)
+
+console.log("Playing as Rock:")
+console.log(playRound("Rock", computerChoice));
+
+console.log("Playing as Paper:")
+console.log(playRound("Paper", computerChoice));
+
+console.log("Playing as Scissors:")
+console.log(playRound("Scissors", computerChoice));
