b33f0a68b5cf7209c94e4fbebd5fa183a2a77ab3
freeram <kulikovpavel2801@gmail.com>
2024-05-13 17:59:11 -0600

    Improve Rock Paper Scissors

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -1,20 +1,51 @@
+"use strict";
+
+const buttons = document.querySelectorAll("button");
+const display = document.querySelector(".display");
+const score = document.querySelector(".score");
+
+const maxRound = 5;
+let round = 1;
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => handleGame(button.id));
+});
+
+function handleGame(playerChoice) {
+    if (round > maxRound) return;
+
+    const computerChoice = generateComputerChoice();
+    const roundResult = playRound(playerChoice, computerChoice);
+
+    if (roundResult.startsWith("You win!")) {
+        playerScore++;
+    } else if (roundResult.startsWith("You lose!")) {
+        computerScore++;
+    } else if (roundResult.startsWith("It's a tie!")) {
+        computerScore++;
+        playerScore++;
+    }
+
+    let resultText = `Computer choice: ${computerChoice}. Player choice: ${playerChoice}. ${roundResult}`;
+    let scoreText = `Player score: ${playerScore}, Computer score: ${computerScore}, Round: ${round} out of ${maxRound}`;
+
+    if (round === maxRound) {
+        resultText = gameEnded(playerScore, computerScore);
+    }
+    round++;
+
+    display.textContent = resultText;
+    score.textContent = scoreText;
+}
 
 function generateComputerChoice() {
-    // Return random integer - either 0, 1 or 2
     let choice = Math.floor(Math.random() * 3);
-
-    switch (choice) {
-        case 0:
-            return "rock";
-        case 1:
-            return "paper";
-        case 2:
-            return "scissors";
-    }
+    return ['rock', 'paper', 'scissors'][choice];
 }
 
 function playRound(playerSelection, computerSelection) {
-
     if (playerSelection.toLowerCase() === computerSelection) {
         return "It's a tie!";
     }
@@ -22,13 +53,13 @@ function playRound(playerSelection, computerSelection) {
     let output;
     switch (playerSelection.toLowerCase()) {
         case "rock":
-            output = (computerSelection === "paper") ? "You lose! Paper beats rock." : "You win! Rock beats scissors."
+            output = (computerSelection === "paper") ? "You lose! Paper beats rock." : "You win! Rock beats scissors.";
             break;
         case "paper":
-            output = (computerSelection === "scissors") ? "You lose! Scissors beat paper." : "You win! Paper beats rock."
+            output = (computerSelection === "scissors") ? "You lose! Scissors beat paper." : "You win! Paper beats rock.";
             break;
         case "scissors":
-            output = (computerSelection === "rock") ? "You lose! Rock beats scissors." : "You win! Scissors beat paper."
+            output = (computerSelection === "rock") ? "You lose! Rock beats scissors." : "You win! Scissors beat paper.";
             break;
         default:
             output = "Incorrect player selection";
@@ -36,34 +67,14 @@ function playRound(playerSelection, computerSelection) {
     return output;
 }
 
-function game(getComputerChoice, getPlayerChoice, rounds) {
-    let computerChoice, playerChoice, computerScore = 0, playerScore = 0;
-
-    for (let i = 0; i < rounds; i++) {
-        console.log(`Round ${i + 1}.`);
-
-        computerChoice = getComputerChoice();
-        playerChoice = getPlayerChoice();
-        console.log(`Computer choice: ${computerChoice}, Player choice: ${playerChoice}`);
-
-        let roundResult = playRound(playerChoice, computerChoice);
-        console.log(roundResult);
-
-        if (roundResult.startsWith("You win!")) { playerScore++; }
-        else if (roundResult.startsWith("You lose!")) { computerScore++; }
-        else if (roundResult.startsWith("It's a tie!")) { computerScore++; playerScore++; }
+function gameEnded(playerScore, computerScore) {
+    if (playerScore == computerScore) {
+        return "Game results: It's a tie";
     }
-
-    let result;
-    console.log("Game results:")
-
-    if (playerScore > computerScore) { result = `You win! Computer score: ${computerScore}, Player score: ${playerScore}.`; }
-    else if (playerScore === computerScore) { result = `It's a tie! Computer score: ${computerScore}, Player score: ${playerScore}`; }
-    else { result = `You lost! Computer score: ${computerScore}, Player score: ${playerScore}.`; }
-    console.log(result);
-
-    return result;
+    else if (playerScore > computerScore) {
+        return "Game results: You win!";
+    }
+    else {
+        return "Game results: You lose!";
+    }
 }
-
-
-game(generateComputerChoice, () => "rock", 5);
